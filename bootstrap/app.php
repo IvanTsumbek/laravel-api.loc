bb14c829ced9af8710d67e3e848d0f8b754d0a53
IvanTsumbek <maincumbek@gmail.com>
2025-07-10 21:58:24 +0300

    5 урок API для Category

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,16 +1,34 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
+//добавили выброс ошибки
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Not found.'
+                ], 404);
+            }
+        });
+    })
+//
+
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
